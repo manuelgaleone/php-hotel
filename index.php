913f0a0055b4cdecc13e6bc6bf4bee65644985a5
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-28 15:18:57 +0100

    Fixed with bootstrap in tables

--- index.php
+++ index.php
@@ -62,6 +62,7 @@ $hotels = [
 <html lang="en">
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -69,11 +70,47 @@ $hotels = [
 </head>
 
 <body>
+    <div class="container">
+        <h1 class="text-center py-5">
+            Grand Hotel dell'Emilia Romagna:
+        </h1>
+        <!--Hotel List-->
+        <table class="table my-3">
+            <thead class="thead-dark">
+                <tr class="text-center">
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to Center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <th class="text-center" scope="row"><?php echo $hotel["name"] ?></th>
+                        <td class="text-center"><?php echo $hotel["description"] ?></td>
+                        <td class="text-center"><?php echo $hotel["parking"] ?></td>
+                        <td class="text-center"><?php echo $hotel["vote"] ?></td>
+                        <td class="text-center"><?php echo $hotel["distance_to_center"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <!--//Hotel List-->
+
+    <!--
     <ul>
         <?php foreach ($hotels as $hotel) : ?>
             <li> <?php echo $hotel["name"] . "<br>" . $hotel["description"] . "<br>" . $hotel["vote"] . "<br>" . $hotel["parking"] . "<br>" . $hotel["distance_to_center"] ?></li>
         <?php endforeach; ?>
     </ul>
+    -->
+
+    <!--Script-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!--//Script-->
 </body>
 
 </html>
